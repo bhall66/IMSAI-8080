209a7553103deb0e1fde7f42201f00ddf2fed1e0
Bruce E. Hall <bhall66@gmail.com>
2025-06-22 14:25:03 -0400

    updated map2
    Added control byte display and improved exit handling.

--- bank switching/map2.asm
+++ bank switching/map2.asm
@@ -1,5 +1,5 @@
 ;
-;   Title:   map4.asm
+;   Title:   map2.asm
 ;  Author:   Bruce E. Hall, w8bh
 ;    Date:   22 June 2025
 ;      HW:   IMSAI 8080esp by TheHighNibble
@@ -8,11 +8,12 @@
 ;   Descr:   use the MPU-B control byte to configure memory
 ;            and display the memory map for 4 different settings
 ;          
-;   Usage:   "DDT map4.hex", then "g8000".  System will halt. 
+;   Usage:   "DDT map2.hex", then "g8000".  System will halt. 
 
 
 .ORG 8000H              ; must stay in RAM
-MBCP .EQU 0F3h          ; MPU-B Control Port
+MBCP    .EQU 0F3h       ; MPU-B Control Port
+BOOTROM .EQU 0D800H     ; bootrom address
 
 start:
     LD   SP,8400h
@@ -38,14 +39,18 @@ start:
     OUT  (MBCP),A       ; change memory configuration  
     CALL map            ; and show it
     
-                        ; OS in memory is trashed, so...
-    JP   0D800h         ; cold boot via bootloader
+    LD   HL,stReboot    ; OS in memory is trashed, so...
+    CALL printStr       ; announce reboot
+    JP   BOOTROM        ; via Bootloader in ROM
 
 
 map:
+    PUSH AF             ; temp save value in A
     LD   HL, stPrefix   ; start output line
     CALL printStr
-    LD   DE, 400H       ; page size of 1K
+    POP  AF             ; retrieve control byte value
+    CALL printHex       ; and display it
+    LD   DE, 400H       ; set memory page size = 1K
     LD   B, 64          ; eval 64 pages of memory (64K)
     LD   HL, 0000       ; starting at 0000h
 rt01:
@@ -78,15 +83,39 @@ printStr:
     INC  HL             ; move to next character
     JR   printStr       ; and loop until done
 
+printHex:
+    PUSH AF 
+    CALL bh1            ; first char = upper nibble
+    POP  AF
+    CALL bh2            ; second char = lower nibble
+    LD   A,' '
+    OUT  (2),A      
+    OUT  (2),A          ; add two spaces at end.   
+    RET
+bh1:	                ; shift right 4 bits
+    RRA
+    RRA
+    RRA
+    RRA
+bh2:	                ; convert lower nibble to Hex
+    OR	  $F0
+    DAA
+    ADD  A, $A0
+    ADC  A, $40
+    OUT  (2),A          ; send character to console
+    RET
+
 stTitle:
     .db 13,10
     .db "  MPU-B Control Byte Test        R=ROM, W=RAM"   ,13,10,13,10
-    .db "| 0000111122223333444455556666777788889999AAAABBBBCCCCDDDDEEEEFFFF |",13,10
-    .db "| 048C048C048C048C048C048C048C048C048C048C048C048C048C048C048C048C |",13,10,0
+    .db "|      0000111122223333444455556666777788889999AAAABBBBCCCCDDDDEEEEFFFF |",13,10
+    .db "| BYTE 048C048C048C048C048C048C048C048C048C048C048C048C048C048C048C048C |",13,10,0
 stPrefix:
-    .db "| ",0
+    .db "|  ",0
 stPostfix:
     .db 27,"[39m",27,"[49m"," |",13,10,0   ; restores default FG/BG colors
+stReboot:
+    .db 13,10,"Rebooting CP/M...",13,10,0
 stROM:
     .db 27,"[41m",27,"[97m",'R',0   ; show white 'R' on red background
 stRAM:

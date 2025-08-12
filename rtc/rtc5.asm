4b03a608022545ee23e6bfbf2a1b4a057b707ead
Bruce E. Hall <bhall66@gmail.com>
2025-08-12 10:12:05 -0400

    rtc5 update
    added comments, reordered code, simplified getTime(), added omitted calcDOW() call.

--- rtc/rtc5.asm
+++ rtc/rtc5.asm
@@ -1,7 +1,7 @@
 ;
 ;   Title:   rtc5.asm
 ;  Author:   Bruce E. Hall, w8bh
-;    Date:   10 Aug 2025
+;    Date:   12 Aug 2025
 ;      HW:   IMSAI8080 emulator by TheHighNibble
 ;      SW:   TASM compiler using Z80 mnemonics (-80 option)
 ;            
@@ -15,7 +15,8 @@ CLKDAT      .EQU    42h         ; RTC data port
             .ORG    100h        ; Run under CP/M
 
 start:      LD   SP,1000h       ; establish local stack  
-            CALL getTime        ; hour/min/sec/days, etc
+            CALL getTime        ; get hour/min/sec/days, etc
+            CALL calcDOW        ; get DOW, too!
             LD   HL,stTime 
             CALL putStr         ; "The time is "
             CALL pTime12        ; print time in 12-hr format
@@ -25,6 +26,37 @@ start:      LD   SP,1000h       ; establish local stack
             CALL pDate          ; then print the date
             CALL crlf               
 fin:        JP   0000           ; return to CP/M
+
+
+; getTime:
+; queries the RTC and saves time variables in memory as
+; sec, min, hour, days, day, month, & year
+;
+getTime:    LD   B,8            ; 8 parameters to retrieve
+            LD   HL,sec         ; point to first param
+jGT:        LD   A,8            
+            SUB  B              ; A = 0 through 7
+            OUT  (CLKCMD),A     ; send the RTC command
+            IN   A,(CLKDAT)     ; and get the RTC result
+            LD   (HL),A         ; store the result
+            INC  HL             ; advance pointer
+            DEC  B              ; for 8080, dont use DJNZ
+            JP   NZ,jGT         ; loop until done            
+            RET 
+
+
+; Calculate the day of the week: 0=Sunday, 1=Monday, etc
+;
+calcDOW:    LD   HL,(days)      ; HL = days since 1/1/78
+            DEC  HL             ; dont count current day
+            LD   DE,7           ; divisor is 7
+            XOR  A              ; clear carry for next op
+jDW:        SBC  HL,DE          ; subtract the divisor
+            JR   NC,jDW         ; keep subtracting until neg
+            ADD  HL,DE          ; undo last subtraction
+            LD   A,L            ; remainder now in HL
+            LD   (dow),A        ; store remainder as day of week
+            RET 
 
 
 ; print current local time in 12-hr (AM/PM) format
@@ -56,7 +88,7 @@ j08:        PUSH HL             ; save AM/PM pointer
 ;
 pDate:      LD   A,(mon)
             INC  A              ; 0-based to 1-based months
-            DAA 
+            DAA                 ; handle BCD math
             CALL pBCD           ; print month
             LD   A,'/'
             CALL putCh
@@ -64,23 +96,10 @@ pDate:      LD   A,(mon)
             CALL pBCD           ; print day 
             LD   A,'/'
             CALL putCh 
-            LD   A,(year)
+            LD   A,(year)       ; has y2K error, fixed in next line
             SUB  0A0h           ; change base 1900 to 2000
             CALL pBCD           ; print year
             RET          
-
-; Calculate the day of the week: 0=Sunday, 1=Monday, etc
-;
-calcDOW:    LD   HL,(days)      ; HL = days since 1/1/78
-            DEC  HL             ; dont count current day
-            LD   DE,7           ; divisor is 7
-            XOR  A              ; clear carry for next op
-jDW:        SBC  HL,DE          ; subtract the divisor
-            JR   NC,jDW         ; keep subtracting until neg
-            ADD  HL,DE          ; undo last subtraction
-            LD   A,L            ; remainder now in HL
-            LD   (dow),A        ; store remainder as day of week
-            RET 
 
 ; Print the day of the week
 ;
@@ -93,58 +112,6 @@ printDOW:   LD   A,(dow)        ; A = day of week
             ADD  HL,BC          ; add in offset
             CALL putStr         ; print day of week
             RET 
-
-
-; getRTC:
-; encapsulates I/O port communication with RTC
-; call with RTC parameter# in A
-; returns with RTC value in A
-;
-getRTC:     OUT  (CLKCMD),A     ; send RTC command
-            IN   A,(CLKDAT)     ; get RTC value
-            RET 
-
-
-; forceDec - change RTC return values from default to BCD to decimal
-; this routine shown for completeness but not needed here.
-;
-;forceDec:  IN   A,(CLKCMD)     ; read RTC format (0=BCD,1=decimal)
-;           OR   A              ; set flags
-;           RET  NZ             ; return if already in decimal
-;           LD   A,0FFh         ; must be in BCD format, so
-;           OUT  (CLKCMD),A     ; toggle format: BCD->decimal  
-;           RET 
-
-
-; getTime:
-; queries the RTC and saves time variables in memory as
-; hour, min, sec, days, month, day, & year
-;
-getTime:    LD   A,0              
-            CALL getRTC         ; RTC 0 = seconds
-            LD   (sec),A        ; save seconds           
-            LD   A,1
-            CALL getRTC         ; RTC 1 = minutes
-            LD   (min),A        ; save minutes                       
-            LD   A,2
-            CALL getRTC         ; RTC 2 = hours
-            LD   (hour),A       ; save hours  
-            LD   A,3 
-            CALL getRTC         ; RTC 3 = days/LSB
-            LD   (days),A       ; save days LSB        
-            LD   A,4
-            CALL getRTC         ; RTC 4 = days/MSB
-            LD   (days+1),A     ; save days MSB   
-            LD   A,5 
-            CALL getRTC         ; RTC 5 = day of month
-            LD   (day),A        ; save day   
-            LD   A,6
-            CALL getRTC         ; RTC 6 = month 
-            LD   (mon),A        ; save month 
-            LD   A,7 
-            CALL getRTC         ; RTC 7 = year 
-            LD   (year),A       ; save year
-            RET         
 
 
 ; write a null-terminated ASCII string, pointed to by HL 
@@ -186,13 +153,18 @@ jBC:        AND  0FH            ; consider only lower 4 bits
             ADD  A,30H          ; convert value to ASCII  
             JP   putCh          ; output ASCII character
 
-sec:        .db 00
-min:        .db 00    
-hour:       .db 00
-days:       .dw 0000
-day:        .db 00
-mon:        .db 00
-year:       .dw 0000
+
+; RTC variables follow
+; they are loaded as a 8-byte block, so keep them together in same order
+sec:        .db 00              ; stored as BCD
+min:        .db 00              ; stored as BCD
+hour:       .db 00              ; stored as BCD
+days:       .dw 0000            ; stored as 16-bit word, days since 1/1/78
+day:        .db 00              ; stored as BCD
+mon:        .db 00              ; stored as BCD, Jan=0, Feb=1, etc
+year:       .dw 00              ; stored as pseudo-BCD with Y2K error
+
+; addition variables follow
 dow:        .db 00
 stTime:     .db "The current time is ",0
 stDow:      .db "Sun",0,"Mon",0,"Tue",0,"Wed",0,"Thu",0,"Fri",0,"Sat",0 
